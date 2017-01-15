86c67e9eafd995fb40a024f58dc2ffc5ee70f1ce
huangzhike <heavenlystate@163.com>
2017-01-15 21:55:37 +0800

    :)

--- js/spa.js
+++ js/spa.js
@@ -1,28 +1,22 @@
 (function() {
 
 	// SPA
-
 	function SPA() {
 		// 存放注册的页面
 		this.routers = {};
 		// 监听hash事件
 		window.addEventListener("hashchange", this.refresh.bind(this), false);
 
-		var that = this;
-		var spaBack = document.getElementById("spa-back");
-
 		// 后退按钮点击
+		var spaBack = document.getElementById("spa-back");
 		spaBack.addEventListener("click", function() {
-
 			// 改变hash触发后退
-			location.hash = "" + prePartial;
-
-
+			location.hash = prePartial;
 		}, false);
 	}
 
-	// 是否返回上一页，上一页，当前页
-	var goBack, prePartial, nowPartial;
+	// 是否返回上一页，当前页，上一页
+	var goBack, nowPartial, prePartial = "home";
 
 	// 注册分页
 	SPA.prototype.route = function(partial, path, req, callback) {
@@ -41,7 +35,6 @@
 		}
 
 		return this;
-
 	};
 
 	// hashchange后更新页面，非常简单的路由
@@ -54,11 +47,7 @@
 			partial = "home";
 			location.hash = "home";
 			// 防止在首页重定向时进行两次请求
-
-			if (prePartial) {
-				return;
-			}
-
+			if (prePartial) return;
 		}
 
 		// 如果点击的是之前的页面或首页就回退
@@ -72,9 +61,9 @@
 		goBack = false;
 
 		// 从回退的页面请求新页面，回退的旧页面移走
-		removePage.call(this, "spa-old", "(0, 0, 0)", prePartial);
+		remove.call(this, "spa-old", "(0, 0, 0)", prePartial);
 		// 从非回退的页面请求新页面，原来的页面移走
-		removePage.call(this, "spa-new", "(-200%, 0, 0)", prePartial);
+		remove.call(this, "spa-new", "(-200%, 0, 0)", prePartial);
 
 		// 异步加载js
 		if (this.routers[partial].js[this.routers[partial].js.length]) {
@@ -93,11 +82,9 @@
 		});
 
 		return this;
-
 	};
 
-	// 页面回退，只支持一级回退
-	// 后退的逻辑是这样的：
+	// 页面回退，只支持一级回退，逻辑是这样的：
 	// 有home和back以及点击之前页面回退三种
 	// back是退到上次点击的页面
 	// home是退到home页，因为只支持一级的回退，点击home回退后再点击back是无效的，因为已经回退了
@@ -113,70 +100,39 @@
 			return;
 		}
 		// 点击首页回退
-		if (partial == "home") {
-			prePartial = partial;
-		}
+		if (partial == "home") prePartial = partial;
+
 		goBack = true;
 		console.log("go to previous page");
 
 		// 从回退的页面推到首页，回退的旧页面移走
-		removePage.call(this, "spa-old", "(200%, 0, 0)", nowPartial);
+		remove.call(this, "spa-old", "(200%, 0, 0)", nowPartial);
 		// 或者从新页退到旧页面
-		removePage.call(this, "spa-new", "(100%, 0, 0)", nowPartial);
-
-		var that = this;
+		remove.call(this, "spa-new", "(100%, 0, 0)", nowPartial);
 
 		// 异步加载js
-
 		if (this.routers[partial].js[this.routers[partial].js.length]) {
 			this.require(partial);
 		}
 
 		// 请求上一页
-		console.log(prePartial)
+		var that = this;
+
 		ajax("GET", this.routers[prePartial].path, "", function(goBack, page) {
 			// ajax完成回调
-			// 上一页的回调
+			that.routers[prePartial].callback(prePartial);
+			that.callbackAnimation(goBack, page, prePartial);
 
-			if (prePartial) {
-				that.routers[prePartial].callback(prePartial);
-				that.callbackAnimation(goBack, page, prePartial);
-
-				// 更新当前页面
-				nowPartial = partial;
-			}
-
+			// 更新当前页面
+			nowPartial = partial;
 		});
-	};
-
-	// ajax请求函数
-
-	function ajax(method, path, data, callback) {
-
-		var XHR = new XMLHttpRequest();
-		XHR.open(method, path, true);
-		if (method === "POST") {
-			// POST请求要定义请求头
-			XHR.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		}
-		// GET为空
-		XHR.send(data);
-		XHR.onreadystatechange = function() {
-			if (XHR.readyState == 4 && XHR.status == 200) {
-				// ajax完成的回调
-				// console.log("responseText: "+ XHR.responseText);
-				callback(goBack, XHR.responseText);
-
-			}
-		}
-
 	};
 
 	// ajax完成后插入新页面
 	SPA.prototype.callbackAnimation = function(goBack, page, partial) {
 		// 如果是回退，创建回退的页面，否则创建新请求的页面
-		goBack ? createPage.call(this, page, "spa-old", "(100%, 0, 0)", partial) : createPage.call(this, page, "spa-new", "(-100%, 0, 0)", partial);
-		console.log("callback done");
+		goBack ? create.call(this, page, "spa-old", "(100%, 0, 0)", partial) : create.call(this, page, "spa-new", "(-100%, 0, 0)", partial);
+		console.log("created");
 	};
 
 	// 从别的page再回到原来page时，因为原来的page已经removeChild，DOM绑定的事件也没了
@@ -202,7 +158,7 @@
 				// 注意是相对主页的路径，不是加载页的路径
 				s.src = js[i];
 				s.async = true;
-				// 闭包，获取i的值
+				// 闭包，获取i值
 				s.onload = (function(i) {
 					return function() {
 						that.routers[partial].js[i] = true;
@@ -215,13 +171,11 @@
 
 	var p = document.getElementById("spa-page");
 
-	// loading动画，pp助手上截的
-	// 可以在F12 Network里throttling模拟低网速的动画
+	// loading动画，pp助手上截的，可以在F12 Network里throttling模拟低网速的动画
 	var loading = document.getElementById("spa-loading");
 
 	// 创建页面
-
-	function createPage(page, cls, trans, partial) {
+	function create(page, cls, trans, partial) {
 
 		var a = this.routers[partial].temp || document.createElement("div");
 		a.classList.remove("spa-old", "spa-new");
@@ -229,9 +183,7 @@
 		a.style.transform = "translate3d(0, 0, 0)";
 		a.setAttribute("class", cls);
 		a.id = cls;
-		if (!this.routers[partial].temp) {
-			a.innerHTML = page;
-		}
+		if (!this.routers[partial].temp) a.innerHTML = page;
 		p.appendChild(a);
 		loading.style.display = "none";
 		// reflow触发动画
@@ -245,7 +197,7 @@
 
 	// 移除页面
 
-	function removePage(cls, trans, pa) {
+	function remove(cls, trans, partial) {
 
 		var a = document.getElementById(cls);
 		if (!a) return;
@@ -260,12 +212,29 @@
 
 		// 因为移除不是即时的，如果超快速点击切换页面可能有bug
 		setTimeout(function() {
-			console.log(pa);
-			that.routers[pa].temp = p.removeChild(a);
-
-			console.log(that.routers[pa].temp);
+			that.routers[partial].temp = p.removeChild(a);
 		}, 400);
 
+	}
+
+	// ajax请求函数
+	function ajax(method, path, data, callback) {
+
+		var XHR = new XMLHttpRequest();
+		XHR.open(method, path, true);
+		if (method === "POST") {
+			// POST请求要定义请求头
+			XHR.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		}
+		// GET为空
+		XHR.send(data);
+		XHR.onreadystatechange = function() {
+			if (XHR.readyState == 4 && XHR.status == 200) {
+				// ajax完成回调
+				// console.log("responseText: "+ XHR.responseText);
+				callback(goBack, XHR.responseText);
+			}
+		}
 	}
 
 	window.SPA = SPA;
